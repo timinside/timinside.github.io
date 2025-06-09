df6017ddee791b06c53e8085e11ff7716be4af4c
BlueZero <timkainside@gmail.com>
2025-06-09 14:39:45 +0300

    Delete programs directory

--- programs/install/XTweakerSetup.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-@title XTweaker Downloading Manager
-powershell -command "Start-Process powershell -ArgumentList 'iwr https://gobobdev.github.io/programs/XTweaker.ps1 -UseBasicParsing | iex' -Verb RunAs"
--- programs/install/XTweakerSetupBeta.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-@title XTweaker Downloading Manager
-powershell -command "Start-Process powershell -ArgumentList 'iwr https://gobobdev.github.io/programs/XTweakerBeta.ps1 -UseBasicParsing | iex' -Verb RunAs"
